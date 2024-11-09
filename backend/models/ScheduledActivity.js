a0be95bb7a9b4964989c0d9de9c55bd59f9040df
Jayesh Savaliya <123955234+jayesh9747@users.noreply.github.com>
2024-11-09 19:19:53 +0530

    Revert "backend and AI model ssetup (#3)"
    This reverts commit 1d819ded0be30c12e1a7037e690fc806e2f6c1a5.

--- backend/models/ScheduledActivity.js
+++ backend/models/ScheduledActivity.js
@@ -1,15 +1,10 @@
 const mongoose = require("mongoose");
 const { CONFIG } = require("../constants/config");
 
-const ScheduledActivitySchema = new mongoose.Schema({
-    parentId: {
+const ScheduleActivitySchema = new mongoose.Schema({
+    userId: {
         type: mongoose.Types.ObjectId,
-        ref: 'user',
-        required: true,
-    },
-    childId: {
-        type: mongoose.Types.ObjectId,
-        ref: 'user',
+        ref: 'user', // Reference to the child user
         required: true,
     },
     activityIds: [{
@@ -18,34 +13,20 @@ const ScheduledActivitySchema = new mongoose.Schema({
         required: true,
     }],
     scheduledDate: {
-        type: String, 
+        type: Date,
         required: true,
+        default: Date.now,
     },
 }, { timestamps: true });
 
-module.exports = mongoose.model('ScheduledActivity', ScheduledActivitySchema);
 
-
-
-ScheduledActivitySchema.pre('save', async function (next) {
-    try {
-        const parent = await mongoose.model('user').findById(this.parentId);
-        if (!parent || parent.accountType !== CONFIG.ACCOUNT_TYPE.PARENT) {
-            const error = new Error('Scheduled activities must be associated with a parent account.');
-            return next(error);
-        }
-
-        const child = await mongoose.model('user').findById(this.childId);
-        if (!child || child.accountType !== CONFIG.ACCOUNT_TYPE.CHILD) {
-            const error = new Error('Activities can only be associated with children accounts.');
-            return next(error);
-        }
-
-        next();
-    } catch (error) {
+ScheduleActivitySchema.pre('save', async function (next) {
+    const user = await mongoose.model('user').findById(this.userId);
+    if (!user || user.accountType !== CONFIG.ACCOUNT_TYPE.CHILD) {
+        const error = new Error('Activities can only be associated with children accounts.');
         return next(error);
     }
+    next();
 });
 
-
-module.exports = mongoose.model("ScheduledActivity", ScheduledActivitySchema);
+module.exports = mongoose.model("ScheduledActivity", ScheduleActivitySchema);
